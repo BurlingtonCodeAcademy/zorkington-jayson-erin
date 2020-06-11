2ff83244eb689ed919aef8287757e0a5d0bbcfd9
Erin Soggs <erinasoggs@gmail.com>
2020-06-11 14:01:38 -0400

    linked which rooms you can move to and from

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ function ask(questionText) {
 // remember the StateMachine lecture
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 let states = {
-  'roomOne': { canChangeTo: [ 'roomTwo' ] },
-  'roomTwo': { canChangeTo: [ 'roomThree' ] },
-  'roomThree': { canChangeTo: [ 'roomOne' ] }
-};
+  'roomOne': {
+    canChangeTo: ['roomTwo', 'roomFour']
+  },
+  'roomTwo': {
+    canChangeTo: ['roomThree', 'roomFive']
+  },
+  'roomThree': {
+    canChangeTo: ['roomFour', 'roomFive']
+  },
+  'roomFour': {
+    canChangeTo: ['roomFive', 'room2']
+  },
+  'roomFive': { canChangeTo: []}
+}
 
 let currentState = "green";
 
@@ -34,13 +44,16 @@ You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign. Would you like to enter Room 1?`;
   let answer = await ask(welcomeMessage);
-    if (answer.toLowerCase() === "yes") {
-      console.log("Entering Room 1...")
-    } else if (answer.toLowerCase() === "no") {
-      console.log("Ok! You don't have to play. See you next time.")
-      process.exit()
-    } else {
-      console.log("Your answer is invalid. Try again.")
-      start()
-    }
+  if (answer.toLowerCase() === "yes") {
+    console.log("Entering Room 1...")
+  } else if (answer.toLowerCase() === "no") {
+    console.log("Ok! You don't have to play. See you next time.")
+    process.exit()
+  } else {
+    console.log("Your answer is invalid. Try again.")
+    start()
   }
+}
+
+while (userAnswer !== exit)
+
